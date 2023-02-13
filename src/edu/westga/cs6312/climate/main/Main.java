687dbf216381992f0357683d9f01364ca30fa184
Justin Maxwell <jm00724@my.westga.edu>
2023-02-13 15:48:31 -0500

    Re-commit with main

--- src/edu/westga/cs6312/climate/main/Main.java
+++ src/edu/westga/cs6312/climate/main/Main.java
@@ -1,9 +1,22 @@
 package edu.westga.cs6312.climate.main;
 
+import edu.westga.cs6312.climate.interfaces.Sensor;
+import edu.westga.cs6312.climate.model.DailySummary;
+import edu.westga.cs6312.climate.model.WeatherStation;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WeatherStation weatherStation = new WeatherStation("Justin's Weather Station");
+		
+//		DailySummary dailySum1 = new DailySummary(2, 13, 2023);
+//		HourlyMeasurement hour1 = new HourlyMeasurement()
+		
+		System.out.println(weatherStation.getSensors());
+		
+		for (Sensor sensor: weatherStation.getSensors()) {
+			System.out.print(sensor.getName());
+		}
 
 	}
 
